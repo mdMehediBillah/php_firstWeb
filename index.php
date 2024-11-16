33890e42f47588202f78d83c174ffa337e7982b7
mdMehediBillah <mailbillah@gmail.com>
2024-11-16 22:44:07 +0100

    conditional structure add

--- index.php
+++ index.php
@@ -11,91 +11,46 @@
 
 <main>
   <?php 
-  // String operators
-$a = 'Hello';
-$b = 'World';
-$c = $a . ' ' . $b;
-echo $c;
-echo '<br>';
+$bool = true;
+$a = 1;
+$b = 4;
+$c = "1";
 
-// Arithmetic operators
-$x = 10;
-$y = 3;
-$z = $x + $y;
-$math = ($x + $y) * $y;
-echo $z;
-echo '<br>';
-echo $math;
-echo '<br>';
-// remainder
-echo $x % $y;
-echo '<br>';
-
-// Assignment operators
-$d = 5;
-$d += 10;
-// expected output: d= 15
-$d -= 5;
-// expected output: d= 10
-echo $d;
-
-// Comparison operators
-$e = 10;
-$f = 20;
-$g = $e == $f;
-// expected output: false
-$h = $e != $f;
-// expected output: true
-
-// Logical operators
-$i = true;
-$j = false;
-$k = $i && $j;
-// expected output: false
-$l = $i || $j;
-// expected output: true
-
-// Increment/Decrement operators
-$m = 5;
-$m++;
-// expected output: 6
-$n = 5;
-$n--;
-// expected output: 4
-
-// Conditional operators
-$o = 20;
-$p = 20;
-$q = ($o > $p) ? 'greater' : 'lesser';
-// expected output: lesser
 echo "<br>";
-if ($o > $p) {
-  echo 'greater';
+$result = match ($c) {
+  1, 3, 5 => "The number is odd" ,
+  2, 4, 6 => "The number is even" ,
+  default => "The number is not 1, 2, 3, 4, 5, or 6",
+};
+echo $result;
+
+echo "<br>";
+
+// swith statement take a single value and it can compare it to multiple values or conditions
+switch ($a){
+  case $a > $b:
+    echo 'a is greater than b';
+    break;
+  case $a < $b:
+    echo 'a is less than b';
+    break;
+  case $a === $b:
+    echo 'a is equal to b';
+    break;
+  default:
+    echo 'a is not greater than, less than, or equal to b';
+}
+
+echo '<br>';
+// if statement can compare multiple values or conditions
+if ($a > $b && !$bool){
+  echo 'The first condition is true';
+} else if ($a < $b && !$bool || !$bool){
+  echo 'The second condition is true';
 } else {
-  echo 'lesser';
-}
-echo "<br>";
-
-$aa = 2;
-$bb = "2";
-// compare values by type value
-if($aa === $bb) {
-  echo 'They are equal';
-}else {
-  echo 'The are not equal';
+  echo 'Both conditions are false';
 }
 
-
-// Null coalescing operator
-$r = null;
-$s = $r ?? 'default';
-// expected output: default
-
-// Spaceship operator
-$t = 10;
-$u = 20;
-$v = $t <=> $u;
-// expected output: -1
 ?>
 
 </main>
