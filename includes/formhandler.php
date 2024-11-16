f1aa7a6dc7c0ce88c6d4a52787c239ef2b8ed908
mdMehediBillah <mailbillah@gmail.com>
2024-11-16 14:18:04 +0100

    formData validate and print it on anothere page

---
+++ includes/formhandler.php
@@ -0,0 +1,29 @@
+<?php
+
+// var_dump($_POST);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $firstName = htmlspecialchars( $_POST['firstName']);
+  $lastName = htmlspecialchars( $_POST['lastName']);
+  $favouritePet = htmlspecialchars( $_POST['favouritepet']);
+
+  if (empty($firstName) || empty($lastName)) {
+    header('Location: ../index.php?error=emptyfields');
+    echo 'Please fill in all fields';
+    exit();
+  }
+  if ($_GET['error'] === 'invalidname') {
+    echo '<p style="color: red;">Invalid name entered.</p>';
+  }
+
+  if ($favouritePet === 'none') {
+    $favouritePet = 'no pet';
+  }
+
+  echo "Hello, $firstName $lastName! Your favourite pet is a $favouritePet!";
+
+  // header('Location: ../index.php');
+}
+else {
+  header('Location: ../index.php');
+}
--- index.php
+++ index.php
@@ -4,84 +4,42 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="./css/main.css">
 </head>
 <body>
 
-<!-- <?php 
-// Scalar Data Types
-$string = "Mehedi Billah";
-$number = 10;
-$integer = 10;
-$float = 10.5;
-$boolean = true;
 
-// Compound Data Types
-$array = array("HTML", "CSS", "JavaScript");
-$array2 = ["HTML", "CSS", "JavaScript"];
-$object = new stdClass();
-
-// Special Data Types
-$null = null;
-
-// Default Data Types
-$name = "";
-$age = 0;
-$float = 0.0;
-$boolean = false;
-$array = [];
-$object= null;
-
-echo $string;
-?>
-
-<p>Hi! my name is <?php echo $string ?>, and I'm learning PHP. </p> -->
-
-<!-- <?php 
-// Super Global Variables
-echo $_SERVER["DOCUMENT_ROOT"];
-echo "<br>";
-echo $_SERVER["SERVER_NAME"];
-echo "<br>";
-echo $_SERVER["SERVER_ADDR"];
-echo "<br>";
-echo $_SERVER["SERVER_PORT"];
-echo "<br>";
-echo $_SERVER["PHP_SELF"];
-echo "<br>";
-echo $_SERVER["REQUEST_METHOD"];
-echo "<br>";
-$_GET[""];
-$_POST[""];
-$_REQUEST[""];
-$_FILES[""];
-$_COOKIE[""];
-$_SESSION[""];
-$_ENV[""];
-$_SERVER[""];
-
-?> -->
-
-<!-- <?php 
-// Get Method _http://localhost/myWebSite/index.php?nameGet=Mehedi&age=30
-echo $_GET["nameGet"];
-echo $_GET["age"];
-?> -->
 <main>
-  <form action="index.php" method="post">
-<label for="firstName">firstName</label>
-<input type="text" name="firstName" id="firstName">
-<label for="lastName">lastName</label>
-<input type="text" name="lastName" id="lastName">
-<label for="favouritepet">favourite Pet?</label>
-<select name="favouritepet" id="favouritepet">
-  <option value="none">None</option>
-  <option value="cat">Cat</option>
-  <option value="dog">Dog</option>
-  <option value="bird">Bird</option>
-  <option value="fish">Fish</option>
-</select>
-    <button type="submit">Submit</button>
-  </form>
+  <section class="form-wrapper" >
+  <!-- <?php
+  if (isset($_GET['error'])) {
+    if ($_GET['error'] === 'emptyfields') {
+      echo '<p style="color: red; font-size: 1rem;">Please fill in all fields.</p>';
+    }
+  }
+  ?> -->
+    <form action="includes/formhandler.php" method="post">
+    <div>
+      <label for="firstName">firstName</label>
+      <input type="text" name="firstName" id="firstName" placeholder="First name">
+    </div>
+    <div>
+      <label for="lastName">lastName</label>
+      <input type="text" name="lastName" id="lastName" placeholder="Last name">
+    </div>
+    <div>
+      <label for="favouritepet">favourite Pet?</label>
+      <select name="favouritepet" id="favouritepet">
+      <option value="none">None</option>
+      <option value="cat">Cat</option>
+      <option value="dog">Dog</option>
+      <option value="bird">Bird</option>
+      <option value="fish">Fish</option>
+      </select>
+    </div>
+      <button type="submit">Submit</button>
+    </form>
+  </section>
 </main>
 </body>
 </html>
