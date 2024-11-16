f668785973511d296d320ebaacd34c937849d2a6
mdMehediBillah <mailbillah@gmail.com>
2024-11-16 12:41:36 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<!-- <?php 
+// Scalar Data Types
+$string = "Mehedi Billah";
+$number = 10;
+$integer = 10;
+$float = 10.5;
+$boolean = true;
+
+// Compound Data Types
+$array = array("HTML", "CSS", "JavaScript");
+$array2 = ["HTML", "CSS", "JavaScript"];
+$object = new stdClass();
+
+// Special Data Types
+$null = null;
+
+// Default Data Types
+$name = "";
+$age = 0;
+$float = 0.0;
+$boolean = false;
+$array = [];
+$object= null;
+
+echo $string;
+?>
+
+<p>Hi! my name is <?php echo $string ?>, and I'm learning PHP. </p> -->
+
+<!-- <?php 
+// Super Global Variables
+echo $_SERVER["DOCUMENT_ROOT"];
+echo "<br>";
+echo $_SERVER["SERVER_NAME"];
+echo "<br>";
+echo $_SERVER["SERVER_ADDR"];
+echo "<br>";
+echo $_SERVER["SERVER_PORT"];
+echo "<br>";
+echo $_SERVER["PHP_SELF"];
+echo "<br>";
+echo $_SERVER["REQUEST_METHOD"];
+echo "<br>";
+$_GET[""];
+$_POST[""];
+$_REQUEST[""];
+$_FILES[""];
+$_COOKIE[""];
+$_SESSION[""];
+$_ENV[""];
+$_SERVER[""];
+
+?> -->
+
+<?php 
+// Get Method _http://localhost/myWebSite/index.php?nameGet=Mehedi&age=30
+echo $_GET["nameGet"];
+echo $_GET["age"];
+?>
+
+</body>
+</html>
